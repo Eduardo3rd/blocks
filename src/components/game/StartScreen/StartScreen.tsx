d2a837b9c409049aa29322b22708e7ad8784dd65
Eduardo Torrealba <Eduardo3rd@gmail.com>
2025-01-26 19:02:31 -0500

    Update start screen to begin on tap/click instead of enter key

--- src/components/game/StartScreen/StartScreen.tsx
+++ src/components/game/StartScreen/StartScreen.tsx
@@ -7,10 +7,13 @@ interface StartScreenProps {
 
 export const StartScreen: React.FC<StartScreenProps> = ({ onStart }) => {
   return (
-    <div className="absolute inset-0 bg-black flex items-center justify-center">
+    <div 
+      className="absolute inset-0 bg-black flex items-center justify-center cursor-pointer"
+      onClick={onStart}
+    >
       <div className="text-center pixel-text">
         <div className="text-4xl text-blue-500 mb-8">TETRIS</div>
-        <div className="text-xl text-white mb-8">Press ENTER to Start</div>
+        <div className="text-xl text-white mb-8">Tap or Click to Start</div>
         <div className="mt-8">
           <HighScores />
         </div>
